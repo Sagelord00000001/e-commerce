1584dbb33ee9e4eefd12ca67cca91f4dfd9d3dfa
Chukwuebuka <emmanuelmrchukwuebuka@gmail.com>
2025-05-15 08:12:57 +0100

    pull request

--- app/page.js
+++ app/page.js
@@ -1,17 +1,18 @@
 
 
-import ExploreByCategory from "@/components/ui/ExploreByCategory";
+import ExploreByCategory from "@/components/ExploreByCategory";
 import Homepage from "@/components/Homepage";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <ExploreByCategory/>
+   
     <div>
       <div className="bg-[#EEEBFF] h-auto md:px-10 px-5 py-4 ">
         <Navbar />
         <Homepage />
+        <ExploreByCategory/>
       </div>
     </div>
   );
